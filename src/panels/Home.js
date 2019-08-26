8ba49421f21414be3bfff291c31a1611a6aee8de
Yra Moroz <moroz-yra@yandex.ru>
2019-08-27 01:42:10 +0300

    first commit

---
+++ src/panels/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Panel, Button, Cell, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
+import "./Home.css";
+
+const Home = ({ id, go, fetchedUser }) => (
+	<Panel id={id}>
+		<PanelHeader>{fetchedUser && fetchedUser.first_name}</PanelHeader>
+		<Div className="wrapper">
+			<h1 className="mh1">Спасибо за то,</h1>
+			<h2 className="mh2">что ты есть</h2>
+			<p className="heart">❤</p>
+		</Div>
+
+		<Div className="center">
+		<Button level="outline" onClick={go} size="l">Отправить дальше</Button>
+		</Div>
+	</Panel>
+);
+
+Home.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+	fetchedUser: PropTypes.shape({
+		photo_200: PropTypes.string,
+		first_name: PropTypes.string,
+		last_name: PropTypes.string,
+		city: PropTypes.shape({
+			title: PropTypes.string,
+		}),
+	}),
+};
+
+export default Home;
