d47e7874e3ed9746dc33367b918ecc74563b7e95
Yra Moroz <moroz-yra@yandex.ru>
2019-08-29 00:11:21 +0300

    Updates

---
+++ precache-manifest.6f25638e34aa4d2c04574ac20423f471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac98bfe165c8ec1e443b",
+    "url": "/heyclickme/static/js/runtime~main.ea01d390.js"
+  },
+  {
+    "revision": "e7fbea26ed05b33744d6",
+    "url": "/heyclickme/static/js/main.08038be6.chunk.js"
+  },
+  {
+    "revision": "524061b61a39d9311cb9",
+    "url": "/heyclickme/static/js/2.d31c98c9.chunk.js"
+  },
+  {
+    "revision": "e7fbea26ed05b33744d6",
+    "url": "/heyclickme/static/css/main.bbe2038f.chunk.css"
+  },
+  {
+    "revision": "524061b61a39d9311cb9",
+    "url": "/heyclickme/static/css/2.2a011aab.chunk.css"
+  },
+  {
+    "revision": "f8a7a98f1f82735558be26c9b6b0c186",
+    "url": "/heyclickme/index.html"
+  }
+];
